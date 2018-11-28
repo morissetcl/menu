a68bcf1da635cd17a8128334693b70ac1771720f
Clement Morisset <morissetcl87@gmail.com>
2018-11-28 08:05:54 +0100

    get menu from restopolitain

--- lib/scraps/menu_restaurant_restopolitain.rb
+++ lib/scraps/menu_restaurant_restopolitain.rb
@@ -5,26 +5,29 @@ require 'mechanize'
 module Scraps
   class MenuRestaurantRestopolitain
     class << self
-      def get_menu(html_doc, restaurant_id)
+      def get_menu(link, restaurant_id)
         @menu = RestaurantMenu.create!(restaurant_id: restaurant_id)
-        get_menu_data(html_doc).search('.catalog__item > span:first').each do |dish|
+        get_menu_data(link).search('.catalog__item > span:first').each do |dish|
           next if dish.text.include?('/')
           create_dish(@menu, dish)
         end
-        address = get_menu_data(html_doc).css('.restaurant__info > p:first > span').text
+        address = get_menu_data(link).css('.restaurant__info > p:first > span').text
         p address
         get_address(address)
       end
 
       private
 
-      def get_link(html_doc)
-        link = html_doc.css('a:first').first['href']
-         "https://www.restopolitan.com#{link}"
+      def get_link(link)
+        if link.include?('https')
+          link
+        else
+          "https://www.restopolitan.com#{link}"
+        end
       end
 
-      def get_menu_data(html_doc)
-        url = get_link(html_doc)
+      def get_menu_data(link)
+        url = get_link(link)
         p url
         sleep 5
         html_file = open(url).read
--- lib/tasks/get_data_restopolitain.rake
+++ lib/tasks/get_data_restopolitain.rake
@@ -1,20 +1,14 @@
 require 'scraps/menu_restaurant_restopolitain'
-require 'scraps/restaurant_restopolitain'
-require 'scraps/regions_restopolitain'
+require 'scraps/restaurant_name_restopolitain'
 
 namespace :scrap do
-  desc "scrap restaurant"
-  task :restaurant_restopolitain => :environment do
-    Scraps::RestaurantRestopolitain.get_restaurant
-  end
-
   desc "scrap menu"
   task :menu_restaurant_restopolitain => :environment do
     Scraps::MenuRestaurantRestopolitain.get_menu
   end
 
-  desc "scrap region"
-  task :regions_restopolitain => :environment do
-    Scraps::RegionsRestopolitain.get_region
+  desc "scrap restaurant name"
+  task :restaurant_name_restopolitain => :environment do
+    Scraps::RestaurantNameRestopolitain.get_name
   end
 end
