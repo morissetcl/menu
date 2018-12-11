0a33eda74b640bc4ac05c30fc2fce30ff1cb1f40
Clement Morisset <morissetcl87@gmail.com>
2018-12-11 07:37:20 +0100

    ass has_many

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Restaurant < ApplicationRecord
+  has_one :restaurant_menu
+  has_many :dishes, through: :restaurant_menu
+
   validates :name, :slug, presence: true, uniqueness: true
   before_save :format_slug
 
