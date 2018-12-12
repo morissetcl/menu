01f0f6eddf3d3eceff6486c61c5e0193794905d9
Clement Morisset <morissetcl87@gmail.com>
2018-12-12 21:50:05 +0100

    add tags to restopolitain

--- app/services/get_restaurant_menu_restopolitain_service.rb
+++ app/services/get_restaurant_menu_restopolitain_service.rb
@@ -4,8 +4,10 @@ require 'open-uri'
 require 'nokogiri'
 
 class GetRestaurantMenuRestopolitainService
+
   class << self
     def call(link, restaurant_id)
+      @restaurant = Restaurant.find(restaurant_id)
       get_menu(link, restaurant_id)
     end
 
@@ -13,14 +15,22 @@ class GetRestaurantMenuRestopolitainService
 
     def get_menu(link, restaurant_id)
       @menu = RestaurantMenu.create!(restaurant_id: restaurant_id)
+      get_food_tags(link)
       get_menu_data(link).search('.catalog__item > span:first').each do |dish|
         next if dish.text.include?('/')
 
         create_dish(@menu, dish)
       end
       address = get_menu_data(link).css('.restaurant__info > p:first > span').text
-      p address
       get_address(address)
+    end
+
+    def get_food_tags(link)
+      food_type = []
+      get_menu_data(link).css('.tags__tag').each do |food|
+        food_type << food.text
+      end
+      @restaurant.update!(tags: food_type.join(','))
     end
 
     def get_link(link)
