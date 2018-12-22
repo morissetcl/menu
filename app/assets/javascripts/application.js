7a7bc59da6ded896bc35dd03d84da1882eb4a699
Clement Morisset <morissetcl87@gmail.com>
2018-12-22 20:55:33 +0100

    design auth page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require activestorage
-//= require turbolinks
-//= require_tree .
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  M.updateTextFields();
+});
