b880d3e193459669c6e41b28de855e82da437290
Clement Morisset <morissetcl87@gmail.com>
2018-12-22 10:17:36 +0100

    limit activeadmin css to activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  devise_for :users
+
+  # sidekiq monitoring
   require 'sidekiq/web'
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     ActiveSupport::SecurityUtils.secure_compare(username, 'sidekiq') &
         ActiveSupport::SecurityUtils.secure_compare(password, ENV['SIDEKIQ_PASSWORD'])
   end
   mount Sidekiq::Web, at: "/sidekiq"
+
+  # back-offices
+  ActiveAdmin.routes(self)
+
+  # authentication
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  devise_for :users
+
+  # public-routes
+  root to: 'home#show'
 end
