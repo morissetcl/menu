4be896f64b203aafd329a0068ec287954f1d4172
Clement Morisset <morissetcl87@gmail.com>
2018-12-30 16:30:42 +0100

    refactor private nav

--- app/javascript/components/PrivateNavbar.js
+++ app/javascript/components/PrivateNavbar.js
@@ -1,38 +1,69 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
-import { faSearch} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import LiveSearch from './LiveSearch'
 
-class PrivateNavbar extends React.Component{
+// Each logical "route" has two components, one for
+// the sidebar and one for the main area. We want to
+// render both of them in different places when the
+// path matches the current URL.
+const routes = [
+  {
+    path: "/private/1/dashboard",
+    exact: true,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/private/1/dashboard",
+    main: () => <h2>couocu</h2>
+  },
+  {
+    path: "/bouyaka",
+    main: () => <h2>Shoelaces</h2>
+  }
+];
 
-   constructor(props, context) {
-      super(props, context);
-   }
-
-   componentDidMount(){}
-
-   render() {
-      return (
-         <div className="private-nav z-depth-5">
-           <li>
-             <a rel="nofollow" data-method="delete" href="/users/sign_out">
-                <FontAwesomeIcon icon={faSearch} className='icon-nav hide-on-med-and-up' />
-                <span className='hide-on-small-only' >Déconnexion</span>
-             </a>
-           </li>
-         </div>
-      )
-   }
+function Sidebar() {
+  return (
+    <Router>
+      <div className= 'row'>
+        <div className= 'col s2'>
+          <div className='private-nav'>
+            <ul style={{ listStyleType: "none", padding: 0 }}>
+              <li>
+                <Link to="/private/1/dashboard">Recherche</Link>
+              </li>
+              <li>
+                <Link to="/private/1/dashboard">Recherche</Link>
+              </li>
+              <li>
+                <Link to="/bouyaka">Recherche</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='col s10'>
+          <div className='loulou'>
+            <Route
+              path='/private/1/dashboard'
+              component={LiveSearch}
+            />
+          </div>
+        </div>
+        <Route
+          path='/bouyaka'
+          component={Child}
+        />
+      </div>
+    </Router>
+  );
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  var new_row = document.createElement("div");
-  new_row.setAttribute("class", "livesearch-container row" );
+function Child({ match }) {
+  return (
+    <div>
+      <h3>ID: {match.params.id}</h3>
+    </div>
+  );
+}
 
-  ReactDOM.render(
-    <PrivateNavbar />,
-    document.body.appendChild(new_row),
-  )
-});
-
-export default PrivateNavbar
+export default Sidebar;
