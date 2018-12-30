f3df64a80da36b178b01167a912db74458f55e6e
Clement Morisset <morissetcl87@gmail.com>
2018-12-30 17:34:01 +0100

    pass props to PrivateNavbar to component

--- app/javascript/components/PrivateNavbar.js
+++ app/javascript/components/PrivateNavbar.js
@@ -2,68 +2,42 @@ import React, {Component} from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LiveSearch from './LiveSearch'
 
-// Each logical "route" has two components, one for
-// the sidebar and one for the main area. We want to
-// render both of them in different places when the
-// path matches the current URL.
-const routes = [
-  {
-    path: "/private/1/dashboard",
-    exact: true,
-    main: () => <h2>Home</h2>
-  },
-  {
-    path: "/private/1/dashboard",
-    main: () => <h2>couocu</h2>
-  },
-  {
-    path: "/bouyaka",
-    main: () => <h2>Shoelaces</h2>
-  }
-];
 
-function Sidebar() {
-  return (
-    <Router>
-      <div className= 'row'>
-        <div className= 'col s2'>
-          <div className='private-nav'>
-            <ul style={{ listStyleType: "none", padding: 0 }}>
-              <li>
-                <Link to="/private/1/dashboard">Recherche</Link>
-              </li>
-              <li>
-                <Link to="/private/1/dashboard">Recherche</Link>
-              </li>
-              <li>
-                <Link to="/bouyaka">Recherche</Link>
-              </li>
-            </ul>
+class PrivateNavbar extends Component {
+  constructor(props) {
+    super(props);
+  };
+
+  render(){
+    return(
+      <Router>
+        <div className= 'row'>
+          <div className= 'col s2'>
+            <div className='private-nav'>
+              <ul style={{ listStyleType: "none", padding: 0 }}>
+                <li>
+                  <Link to={`/private/${this.props.userId}/dashboard`}>Recherche</Link>
+                </li>
+                <li>
+                  <Link to="/bouyaka">Recherche</Link>
+                </li>
+                <li>
+                  <a href='/users/sign_out' data-method="delete" rel="nofollow">Déconnexion</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className='col s10'>
+            <div className='loulou'>
+              <Route
+                path='/private/:userId/dashboard'
+                component={LiveSearch}
+              />
+            </div>
           </div>
         </div>
-        <div className='col s10'>
-          <div className='loulou'>
-            <Route
-              path='/private/1/dashboard'
-              component={LiveSearch}
-            />
-          </div>
-        </div>
-        <Route
-          path='/bouyaka'
-          component={Child}
-        />
-      </div>
-    </Router>
-  );
+      </Router>
+    )
+  };
 }
-
-function Child({ match }) {
-  return (
-    <div>
-      <h3>ID: {match.params.id}</h3>
-    </div>
-  );
-}
-
-export default Sidebar;
+export default PrivateNavbar;
