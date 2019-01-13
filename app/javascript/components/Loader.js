7519c1eb8ff4d434e5514d593ea865440f360dfc
Clement Morisset <morissetcl87@gmail.com>
2019-01-13 14:15:47 +0100

    add style to loader

--- app/javascript/components/Loader.js
+++ app/javascript/components/Loader.js
@@ -4,14 +4,16 @@ class Loader extends Component {
 
   render(){
     return (
-      <div className="preloader-wrapper big active">
-        <div className="spinner-layer spinner-blue">
-          <div className="circle-clipper left">
-            <div className="circle"></div>
-          </div><div className="gap-patch">
-            <div className="circle"></div>
-          </div><div className="circle-clipper right">
-            <div className="circle"></div>
+      <div className='wrapper-loader'>
+        <div className="preloader-wrapper big active">
+          <div className="spinner-layer spinner-blue">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+            </div><div className="gap-patch">
+              <div className="circle"></div>
+            </div><div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
           </div>
         </div>
       </div>
