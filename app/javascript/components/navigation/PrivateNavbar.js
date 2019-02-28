ae7a6088c1176322d2ad3a6edccb5f57e97486be
Clement Morisset <morissetcl87@gmail.com>
2019-02-28 21:52:02 +0100

    remove jquery, but not fixed toggleclass yet

--- app/javascript/components/navigation/PrivateNavbar.js
+++ app/javascript/components/navigation/PrivateNavbar.js
@@ -6,19 +6,13 @@ import LiveSearch from '../LiveSearch'
 import Restaurant from '../Restaurant'
 import Favorite from '../actions/Favorite'
 import Calendar from '../Calendar'
+import NavigationLinks from './NavigationLinks'
 
 class PrivateNavbar extends Component {
 
   constructor(props) {
     super(props);
   };
-
-  componentDidMount() {
-    $('.navbar-link').click(function () {
-      $('.navbar-link').removeClass("active-link");
-      $(this).addClass("active-link");
-    });
-  }
 
   render(){
     return(
@@ -27,21 +21,18 @@ class PrivateNavbar extends Component {
           <div className= 'col s1 revert-padding-col'>
             <div className='private-nav'>
               <ul style={{ listStyleType: "none", padding: 0 }}>
-                <Link to={`/private/${this.props.userId}/dashboard`}>
-                  <li className='navbar-link'>
-                    <FontAwesomeIcon icon={faSearch}/>
-                  </li>
-                </Link>
-                <Link to={`/private/${this.props.userId}/favorite`}>
-                  <li className='navbar-link'>
-                    <FontAwesomeIcon icon={faStar}/>
-                  </li>
-                </Link>
-                <Link to={`/private/${this.props.userId}/calendar`}>
-                  <li className='navbar-link'>
-                    <FontAwesomeIcon icon={faCalendarWeek}/>
-                  </li>
-                </Link>
+                <NavigationLinks
+                  path={`/private/${this.props.userId}/dashboard`}
+                  icon={faSearch}
+                />
+                <NavigationLinks
+                  path={`/private/${this.props.userId}/favorite`}
+                  icon={faStar}
+                />
+                <NavigationLinks
+                  path={`/private/${this.props.userId}/calendar`}
+                  icon={faCalendarWeek}
+                />
                 <a href='/users/sign_out' data-method="delete" rel="nofollow">
                   <li className='navbar-link'><FontAwesomeIcon icon={faSignOutAlt}/></li>
                 </a>
