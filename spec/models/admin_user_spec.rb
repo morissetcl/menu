fa72890a56150e34f1c9df219ed60c2aaa48c2e8
Clement Morisset <morissetcl87@gmail.com>
2019-03-06 20:28:15 +0100

    add model test for admin

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :email }
+  it { should validate_presence_of :password }
 end
