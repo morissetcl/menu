d74e3856195fd19de6b7d5065307c86b44457760
Clement Morisset <morissetcl87@gmail.com>
2019-03-06 20:45:43 +0100

    remove useless test

--- spec/features/routes/private_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-feature 'Private Navigation', js: true do
-  before { sign_as_user }
-
-  scenario 'coucou', :focus do
-    expect(page).to have_content 'Recherche par nom ou adresse'
-  end
-end
