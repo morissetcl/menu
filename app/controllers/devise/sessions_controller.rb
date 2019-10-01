4a2ed8956292065547163849ed998dc44fec5836
Clement Morisset <morissetcl87@gmail.com>
2019-10-01 07:59:58 +0200

    add style to password reset form

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -23,7 +23,7 @@ module Devise
       sign_in(resource_name, resource)
       yield resource if block_given?
       location = params['admin_user'] ? admin_admin_users_path : after_sign_in_path_for(resource)
-      respond_with resource, location:location
+      respond_with resource, location: location
     end
 
     # DELETE /resource/sign_out
